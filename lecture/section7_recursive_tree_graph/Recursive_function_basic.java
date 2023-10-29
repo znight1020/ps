147b0bfbc298b82d44ec16fb47f4961316935723
현수 <znight1020@naver.com>
2023-10-29 22:41:54 +0900

    07-01. 재귀 함수(theory)

---
+++ lecture/section7_recursive_tree_graph/Recursive_function_basic.java
@@ -0,0 +1,26 @@
+package lecture.section7_recursive_tree_graph;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Recursive_function_basic {
+    public void DFS(int n){
+        if(n == 0) return;
+
+        // etc 폴더 의 StackFrame 참고
+        else{
+            DFS(n - 1); // 오름차순
+            System.out.print(n + " ");
+            // DFS(n - 1); // 내림차순
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        Recursive_function_basic r = new Recursive_function_basic();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        r.DFS(num);
+    }
+}
