1288b50a58c76a138fe0eae4d27380a5cb71f629
이현수 <znight1020@naver.com>
2024-01-30 15:30:45 +0900

    [solved.ac]: N과M 1

---
+++ solved/s3/ClimbingStairs.java
@@ -0,0 +1,11 @@
+package solved.s3;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ClimbingStairs {
+
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
+}
---
+++ solved/s3/NandM.java
@@ -0,0 +1,44 @@
+package solved.s3;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class NandM {
+    static boolean[] vtd;
+    static int N, M;
+    static int[] arr;
+
+    static void perm(int L){
+        if(L == M){
+            for(int x : arr){
+                System.out.print(x + " ");
+            }
+            System.out.println();
+            return;
+        }
+        else
+        {
+            for(int i = 1; i <= N; i++){
+                if(vtd[i]) continue;
+                else{
+                    arr[L] = i;
+                    vtd[i] = true;
+                    perm(L+1);
+                    vtd[i] = false;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        vtd = new boolean[N+1];
+        arr = new int[M];
+        perm(0);
+    }
+}
