6a2561c548bfc6136d6fcfd358b963652f11cb79
현수 <znight1020@naver.com>
2024-01-30 20:11:51 +0900

    [solved.ac]: 계단오르기(Top-down 방식도 해볼 것)

--- solved/s3/ClimbingStairs.java
+++ solved/s3/ClimbingStairs.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ClimbingStairs {
+    static int[] arr;
     static int[] dp;
     static void bottomUp(int[] arr){
-        int cnt = 0;
-        dp[0] = 0;
-        for(int i = 1; i < arr.length; i++){
-
+        dp[1] = arr[1];
+        dp[2] = arr[1] + arr[2];
+        dp[3] = Math.max(arr[1] + arr[3], arr[2] + arr[3]);
+        for(int i = 4; i < arr.length; i++){ // dp[2] = 30
+            dp[i] = Math.max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i];
         }
     }
 
@@ -18,12 +20,14 @@ public class ClimbingStairs {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-        int[] arr = new int[T];
-        dp = new int[T+1];
-        for(int t = 0; t < T; t++){
+        arr = new int[301];
+        dp = new int[301];
+        for(int t = 1; t <= T; t++){
             arr[t] = Integer.parseInt(br.readLine());
         }
 
         bottomUp(arr);
+
+        System.out.println(dp[T]);
     }
 }
