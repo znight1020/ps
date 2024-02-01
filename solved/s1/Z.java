f31b59c6ede5a0ee529beab1389ec94e612a0945
현수 <znight1020@naver.com>
2024-02-01 21:40:15 +0900

    try: Z

--- solved/s1/Z.java
+++ solved/s1/Z.java
@@ -7,39 +7,70 @@ import java.util.StringTokenizer;
 
 public class Z {
     static int answer = 0;
-    static int r, c;
+    static int N, r, c;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        r = Integer.parseInt(st.nextToken()); // i
-        c = Integer.parseInt(st.nextToken()); // j
+        N = Integer.parseInt(st.nextToken());
+        r = Integer.parseInt(st.nextToken()); // 세로
+        c = Integer.parseInt(st.nextToken()); // 가로
 
-        solution((int)Math.pow(2, N) * (int)Math.pow(2, N)); // 8 * 8 = 64
+        int length = (int)Math.pow(2, N);
+
+        if(length == 4) answer = 2 * r + c - 1;
+        else solution(length, length);
+
+        System.out.print(answer);
     }
 
-    static void solution(int length){
-        int temp = length / 4;
-        System.out.println(answer);
+    static void solution(int rLength, int cLength){
+        if((rLength == 1 && cLength == 1) || (r == rLength && c == cLength)) return;
 
+        int tempR = rLength / 2;
+        int tempC = cLength / 2;
+
+        System.out.println("rLen: " + rLength + ", cLen: " + cLength + ", answer: " + answer);
         // 4개로 나누기
-        if(r < length / 2 && c < length / 2) { // 1 사분면
-            solution(length / 2);
-        }
-        else if(r < length / 2 && c >= length / 2) { // 2 사분면
-            answer += temp;
-            solution(length / 2);
-        }
-        else if(r >= length / 2 && c < length / 2) { // 3 사분면
-            answer += temp * 2;
-            solution(length / 2);
-        }
-        else if(r >= length / 2 && c >= length / 2) { // 4 사분면
-            answer += temp * 3;
-            solution(length / 2);
-        }
+            if(r < rLength / 2 && c < cLength / 2) { // 1 사분면 = 점수 없음
+                solution(rLength / 2, cLength / 2);
+            }
+            else if(r < rLength / 2 && c >= cLength / 2) { // 2 사분면 = 1 사분면의 점수 획득
+                answer += tempR * tempC;
+                solution(rLength / 2, cLength / 2);
+            }
+            else if(r >= rLength / 2 && c < cLength / 2) { // 3 사분면 = 1,2 사분면의 점수 획득
+                answer += 2 * tempR * tempC;
+                solution(rLength / 2, cLength / 2);
+            }
+            else if(r >= rLength / 2 && c >= cLength / 2) { // 4 사분면 = 1,2,3 사분면의 점수 획득
+                answer += 3 * tempR * tempC;
+                solution(rLength / 2, cLength / 2);
+            }
+
+
+//        if(rLength == 1 && cLength == 1) return;
+//
+//        int tempR = rLength / 2;
+//        int tempC = cLength / 2;
+//
+//        // 4개로 나누기
+//        if(r < tempR && c < tempC) { // 1 사분면 = 점수 없음
+//            solution(tempR, tempC);
+//        }
+//        else if(r < tempR) { // 2 사분면 = 1 사분면의 점수 획득
+//            answer += tempR * tempC;
+//            solution(tempR, tempC);
+//        }
+//        else if(c < tempC) { // 3 사분면 = 1,2 사분면의 점수 획득
+//            answer += 2 * tempR * tempC;
+//            solution(tempR, tempC);
+//        }
+//        else { // 4 사분면 = 1,2,3 사분면의 점수 획득
+//            answer += 3 * tempR * tempC;
+//            solution(tempR , tempC);
+//        }
 
     }
 }
