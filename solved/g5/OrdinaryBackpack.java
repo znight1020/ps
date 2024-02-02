5b663479877006eefa1d32abfe5c85015acf7e9d
이현수 <znight1020@naver.com>
2024-02-02 17:56:17 +0900

    try: 평범한 배낭

--- solved/g5/OrdinaryBackpack.java
+++ solved/g5/OrdinaryBackpack.java
@@ -10,20 +10,27 @@ import java.util.StringTokenizer;
 
 public class OrdinaryBackpack {
     static int answer = 0;
+    static HashMap<Integer, Integer> map = new HashMap<>();
 
-    int solution(int K){
+    static int[][] dp; // i = 가치, j = 무게
+
+    static int solution(int K){
         // 선택된 요소들의 무게의 합이 총 무게를 넘지 않을 때
+        dp[1][0] = 0;
+
+
         return answer;
     }
     public static void main(String[] args) throws IOException{
-        OrdinaryBackpack o = new OrdinaryBackpack();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
-        HashMap<Integer, Integer> map = new HashMap<>();
+
         int weight, value; // 무게, 가치
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()); // 개수
         int K = Integer.parseInt(st.nextToken()); // 최대 무게
+
+        dp = new int[N][K+1]; // i = 가치, j = 무게
 
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
@@ -31,10 +38,7 @@ public class OrdinaryBackpack {
             value = Integer.parseInt(st.nextToken());
             map.put(weight, value);
         }
-        List<Integer> keyList = new ArrayList(map.keySet());
-        //keyList.sort((s1, s2) -> s1.compareTo(s2));
-        keyList.sort(Integer::compareTo);
-        o.solution(K);
+        solution(K);
         
     }
     
