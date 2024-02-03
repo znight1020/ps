3d5f19fb372f687ed0c22a5ff6d5232cd875a058
현수 <znight1020@naver.com>
2024-02-03 14:55:03 +0900

    [solved.ac]: 평범한 배낭
    - 1차원 DP로 해결해보려 했으나, 이전 물품들의 가치를 저장할 수 없었음.
    -> 2차원 DP 문제
    
    - 2차원 DP 문제 연습 + Top-Down 방식으로도 구현해보기

--- solved/g5/OrdinaryBackpack.java
+++ solved/g5/OrdinaryBackpack.java
@@ -3,43 +3,46 @@ package g5;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class OrdinaryBackpack {
     static int answer = 0;
-    static HashMap<Integer, Integer> map = new HashMap<>();
+    static int N, K;
 
-    static int[][] dp; // i = 가치, j = 무게
+    static int[][] dp;
 
-    static int solution(int K){
-        // 선택된 요소들의 무게의 합이 총 무게를 넘지 않을 때
-        dp[1][0] = 0;
+    // static List<Integer> weightList = new ArrayList<>();
+    // static List<Integer> valueList = new ArrayList<>();
 
-
-        return answer;
-    }
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        int weight, value; // 무게, 가치
         st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken()); // 개수
-        int K = Integer.parseInt(st.nextToken()); // 최대 무게
+        N = Integer.parseInt(st.nextToken()); // 개수
+        K = Integer.parseInt(st.nextToken()); // 최대 무게
+        int max = Integer.MIN_VALUE;
 
-        dp = new int[N][K+1]; // i = 가치, j = 무게
+        dp = new int[N+1][K+1];
 
-        for(int i = 0; i < N; i++){
+        int weight, value;
+        for(int i = 1; i < N+1; i++){
             st = new StringTokenizer(br.readLine());
             weight = Integer.parseInt(st.nextToken());
             value = Integer.parseInt(st.nextToken());
-            map.put(weight, value);
+
+            // 현재 무게에서, 새로 들어온 물품의 무게가 K보다 작은경우, 새로운 물품의 가치가 추가 된 무게를 저장한다.
+            // i(새로운 물품)이 갱신되면, dp[i][j]는 if 문에 의해 이전까지 새로운 들어온 물품들의 무게 합이 K보다 작은 경우의 최댓값이 들어있고, dp[i-1][j]와 비교하여 더 큰값을 집어넣는다.
+            // 안넣어도 최댓값을 찾아낼 수는 있다..? -> 안 넣을 경우 마지막줄의 갱신이 이루어지지 않음
+            for(int j = 0; j < K+1; j++){
+                if(j + weight <= K) dp[i][j+weight] = dp[i-1][j] + value;
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j]);
+            }
         }
-        solution(K);
-        
+
+
+        System.out.print(Arrays.stream(dp[N]).max().getAsInt());
+
     }
     
 }
