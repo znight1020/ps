6c8f87cfca2a26ebeed8acbd5657ee93e0c0c4bb
이현수 <znight1020@naver.com>
2024-02-28 12:06:39 +0900

    [solved.ac]: RGB 거리2

--- solved/g4/RGBDistance_DP.java
+++ solved/g4/RGBDistance_DP.java
@@ -1,9 +1,8 @@
-package solved.s1;
+package solved.g4;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class RGBDistance_DP {
