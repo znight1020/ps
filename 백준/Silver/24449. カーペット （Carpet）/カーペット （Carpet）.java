be218b4e3d950fecf0bb0d697c5a461db7dcbc73
현수 <104980470+znight1020@users.noreply.github.com>
2024-03-26 10:33:04 +0900

    [Silver I] Title: カーペット (Carpet), Time: 332 ms, Memory: 66688 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24449. カーペット （Carpet）/カーペット （Carpet）.java
+++ 백준/Silver/24449. カーペット （Carpet）/カーペット （Carpet）.java
@@ -1,56 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static char[][] map;
     static int[] dy = {-1,1,0,0};
     static int[] dx = {0,0,-1,1};
+    static int N, M;
+    static char[][] map;
+    static int[][] dis;
     static boolean[][] vtd;
-    static int H, W, answer = Integer.MAX_VALUE;
-    static Point startPoint = new Point(0,0);
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        H = Integer.parseInt(st.nextToken());
-        W = Integer.parseInt(st.nextToken());
-        map = new char[H][W];
-        vtd = new boolean[H][W];
-        for(int i = 0; i < H; i++) map[i] = br.readLine().toCharArray();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        DFS(startPoint, 0, new boolean[H][W]);
-        vtd[0][0] = true;
-        if(answer == Integer.MAX_VALUE) System.out.print(-1);
-        else System.out.print(answer);
+        map = new char[N][M];
+        dis = new int[N][M];
+        vtd = new boolean[N][M];
+
+        for(int i = 0; i < N; i++) map[i] = br.readLine().toCharArray();
+
+        BFS();
+        if(dis[N-1][M-1] != 0)System.out.print(dis[N-1][M-1]);
+        else System.out.print(-1);
     }
 
-    static void DFS(Point p, int cnt, boolean[][] vtd){
-        char current = map[p.y][p.x];
-        if(cnt >= answer) return;
-        if(p.y == H-1 && p.x == W-1) {
-            answer = Math.min(answer, cnt);
-            return;
-        }
+    static void BFS(){
+        Queue<Point> pointQueue = new LinkedList<>();
+        pointQueue.add(new Point(0,0, map[0][0]));
 
-        for(int i = 0; i < 4; i++){
-            int ny = p.y + dy[i];
-            int nx = p.x + dx[i];
+        while(!pointQueue.isEmpty()){
+            Point p = pointQueue.poll();
+            if(p.y == N-1 && p.x == M-1) return;
 
-            if(ny < 0 || ny >= H || nx < 0 || nx >= W || vtd[ny][nx] || map[ny][nx] == current) continue;
-            vtd[ny][nx] = true;
-            DFS(new Point(ny, nx), cnt + 1, vtd);
-            vtd[ny][nx] = false;
+            if(vtd[p.y][p.x]) continue;
+            vtd[p.y][p.x] = true;
+
+            for(int i = 0; i < 4; i++){
+                int ny = p.y + dy[i];
+                int nx = p.x + dx[i];
+
+                if(ny < 0 || ny >= N || nx < 0 || nx >= M || p.value == map[ny][nx]) continue;
+                dis[ny][nx] = dis[p.y][p.x] + 1;
+                pointQueue.add(new Point(ny, nx, map[ny][nx]));
+            }
         }
     }
 
     static class Point{
         int y, x;
+        char value;
 
-        public Point(int y, int x) {
+        public Point(int y, int x, char value) {
             this.y = y;
             this.x = x;
+            this.value = value;
         }
     }
 }
