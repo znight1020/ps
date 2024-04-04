7c6d9a8915456f7a6f7ed6da9859bdaf8c0b1375
현수 <104980470+znight1020@users.noreply.github.com>
2024-04-04 14:36:13 +0900

    [Gold II] Title: 색종이 붙이기, Time: 204 ms, Memory: 45260 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/README.md
+++ 백준/Gold/17136. 색종이 붙이기/README.md
@@ -4,7 +4,7 @@
 
 ### 성능 요약
 
-메모리: 54788 KB, 시간: 304 ms
+메모리: 45260 KB, 시간: 204 ms
 
 ### 분류
 
@@ -12,13 +12,13 @@
 
 ### 제출 일자
 
-2024년 3월 3일 04:15:03
+2024년 4월 4일 11:46:42
 
 ### 문제 설명
 
 <p><그림 1>과 같이 정사각형 모양을 한 다섯 종류의 색종이가 있다. 색종이의 크기는 1×1, 2×2, 3×3, 4×4, 5×5로 총 다섯 종류가 있으며, 각 종류의 색종이는 5개씩 가지고 있다.</p>
 
-<p style="text-align: center;"><img alt="" src="https://upload.acmicpc.net/496452ae-ce36-4d77-93f7-19d7f3f9ce28/-/preview/" style="width: 972px; height: 302px;"></p>
+<p style="text-align: center;"><img alt="" src="" style="width: 972px; height: 302px;"></p>
 
 <p style="text-align: center;"><그림 1></p>
 
--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -4,70 +4,64 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] paper = {5,5,5,5,5};
+    static int[] paper = {5, 5, 5, 5, 5};
     static int[][] board = new int[10][10];
     static int answer = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
 
-        for(int i = 0; i < 10; i++){
-            st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < 10; j++) board[i][j] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 10; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 10; j++) board[i][j] = Integer.parseInt(st.nextToken());
         }
-        DFS(0, new Point(0,0));
-        if(answer == Integer.MAX_VALUE) System.out.print(-1);
-        else System.out.print(answer);
+
+        DFS(0, new Point(0, 0));
+        System.out.print((answer == Integer.MAX_VALUE) ? -1 : answer);
     }
 
-    static void DFS(int cnt, Point p){
-        if(p.y == 10 && p.x == 0){
+    static void DFS(int cnt, Point p) {
+        if (p.y == 10 && p.x == 0) {
             answer = Math.min(cnt, answer);
             return;
         }
-        if(cnt >= answer) return;
+        if (cnt >= answer) return;
 
-        if(board[p.y][p.x] == 1){
-            for(int i = 5; i >= 1; i--){
-                if(check(p, i)) {
+        if (board[p.y][p.x] == 1) {
+            for (int i = 5; i >= 1; i--) {
+                if (check(p, i)) {
                     attachOrDetach(p, i, 0);
-                    paper[i-1]--;
+                    paper[i - 1]--;
 
-                    if(p.x + 1 == 10) DFS(cnt+1, new Point(p.y+1, 0));
-                    else DFS(cnt+1, new Point(p.y, p.x+1));
+                    Point next = (p.x + i == 10) ? new Point(p.y + 1, 0) : new Point(p.y, p.x + 1);
+                    DFS(cnt + 1, next);
 
                     attachOrDetach(p, i, 1);
-                    paper[i-1]++;
+                    paper[i - 1]++;
                 }
             }
-        }
-        else{
-            if(p.x + 1 == 10) DFS(cnt, new Point(p.y+1, 0));
-            else DFS(cnt, new Point(p.y, p.x+1));
-        }
-    }
-
-    static void attachOrDetach(Point p, int len, int x){
-        for(int i = p.y; i < p.y + len; i++){
-            for(int j = p.x; j < p.x + len; j++){
-                board[i][j] = x;
-            }
+        } else {
+            Point next = (p.x + 1 == 10) ? new Point(p.y + 1, 0) : new Point(p.y, p.x + 1);
+            DFS(cnt, next);
         }
     }
 
-    static boolean check(Point p, int len){
-        if(p.y + len > 10 || p.x + len > 10) return false;
-
-        for(int i = p.y; i < p.y + len; i++) {
-            for(int j = p.x; j < p.x + len; j++) {
-                if(board[i][j] == 0) return false;
-            }
-        }
-        return paper[len-1] > 0;
+    static void attachOrDetach(Point p, int len, int x) {
+        for (int i = p.y; i < p.y + len; i++) for (int j = p.x; j < p.x + len; j++) board[i][j] = x;
     }
-    static class Point{
+
+    static boolean check(Point p, int len) {
+        if (p.y + len > 10 || p.x + len > 10) return false;
+        for (int i = p.y; i < p.y + len; i++)
+            for (int j = p.x; j < p.x + len; j++)
+                if (board[i][j] == 0) return false;
+
+        return paper[len - 1] > 0;
+    }
+
+    static class Point {
         int y, x;
+
         public Point(int y, int x) {
             this.y = y;
             this.x = x;
