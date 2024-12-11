6962c0646495c70008a9a07336eeafb048da7b48
이현수 <znight1020@naver.com>
2024-12-12 02:56:11 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 3084 ms, Memory: 114604 KB -BaekjoonHub

---
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+  static int N, answer = 0, max = 0;
+  static List<Student> students = new ArrayList<>();
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    N = Integer.parseInt(br.readLine());
+    for(int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int c1 = Integer.parseInt(st.nextToken());
+      int c2 = Integer.parseInt(st.nextToken());
+      int c3 = Integer.parseInt(st.nextToken());
+      int c4 = Integer.parseInt(st.nextToken());
+      int c5 = Integer.parseInt(st.nextToken());
+      Student student = new Student(i+1, List.of(c1,c2,c3,c4,c5));
+      students.add(student);
+    }
+
+    for(int i = 0; i < N; i++) {
+      Student s = students.get(i);
+      List<Integer> within = new ArrayList<>();
+      for(int j = 0; j < 5; j++) {
+        int standard = s.classroom.get(j);
+
+        for(int k = 0; k < N; k++) {
+          Student comparison = students.get(k);
+          if(standard == comparison.classroom.get(j) && !within.contains(comparison.num)) {
+            within.add(comparison.num);
+          }
+        }
+      }
+
+      if(within.size() > max) {
+        max = within.size();
+        answer = s.num;
+      }
+    }
+
+    System.out.println(answer);
+  }
+
+  static class Student {
+    int num;
+    List<Integer> classroom;
+
+    Student(int num, List<Integer> classroom) {
+      this.num = num;
+      this.classroom = classroom;
+    }
+  }
+}
