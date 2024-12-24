3ed05d40e03d733bfdedf22758ff0cc463b205d1
이현수 <znight1020@naver.com>
2024-12-25 02:59:16 +0900

    [Bronze V] Title: AI 선도대학, Time: 100 ms, Memory: 14236 KB -BaekjoonHub

---
+++ 백준/Bronze/32951. AI 선도대학/AI 선도대학.java
@@ -0,0 +1,7 @@
+import java.io.*;
+public class Main {
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    System.out.print(Integer.parseInt(br.readLine()) - 2024);
+  }
+}
