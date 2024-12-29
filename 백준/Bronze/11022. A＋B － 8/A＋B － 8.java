b7a54883c08b1f045c9d99a3fbc14745aafa27d5
이현수 <znight1020@naver.com>
2024-12-30 00:05:15 +0900

    [Bronze V] Title: A+B - 8, Time: 116 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int N = Integer.parseInt(br.readLine());
+
+    for(int i = 0; i < N; i++) {
+      StringTokenizer st = new StringTokenizer(br.readLine());
+      int n1 = Integer.parseInt(st.nextToken());
+      int n2 = Integer.parseInt(st.nextToken());
+      sb.append("Case #").append(i+1).append(": ").append(n1).append(" + ").append(n2).append(" = ").append(n1+n2).append("\n");
+    }
+    System.out.print(sb);
+  }
+}
