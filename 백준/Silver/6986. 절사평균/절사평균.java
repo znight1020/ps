5bb3d5886237d6fa8fc61e51e03e849871e62ad0
이현수 <znight1020@naver.com>
2025-01-06 12:21:32 +0900

    [Silver III] Title: 절사평균, Time: 420 ms, Memory: 36448 KB -BaekjoonHub

--- 백준/Silver/6986. 절사평균/절사평균.java
+++ 백준/Silver/6986. 절사평균/절사평균.java
@@ -7,28 +7,23 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        double sum = 0.0;
 
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        for(int i = 0; i < N; i++) numList.add(Double.parseDouble(br.readLine()));
+        for(int i = 0; i < N; i++) {
+            double num = Double.parseDouble(br.readLine());
+            numList.add(num);
+            sum += num;
+        }
         Collections.sort(numList);
 
-        for(int i = 0; i < K; i++) {
-            numList.remove(0);
-            numList.remove(numList.size()-1);
-        }
+        for(int i = 0; i < K; i++) sum -= numList.get(i) + numList.get(numList.size()-1-i);
+        double truncation = (sum + 0.0000001) / (N - K * 2);
 
-        double truncation = Math.round(numList.stream().mapToDouble(Double::doubleValue).average().getAsDouble() * 100) / 100.0;
-
-        double first = numList.get(0);
-        double last = numList.get(numList.size()-1);
-        for(int i = 0; i < K; i++) {
-            numList.add(first);
-            numList.add(last);
-        }
-
-        double correction = Math.round(numList.stream().mapToDouble(Double::doubleValue).average().getAsDouble() * 100) / 100.0;
+        for(int i = 0; i < K; i++) sum += numList.get(K) + numList.get(N-K-1);
+        double correction = (sum + 0.0000001) / N;
 
         System.out.printf("%.2f\n", truncation);
         System.out.printf("%.2f", correction);
