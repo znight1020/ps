a53d2850cc45c171163fd1743bccd1d4d5ec5a22
이현수 <znight1020@naver.com>
2025-01-27 12:05:01 +0900

    [Gold V] Title: 두 수의 합, Time: 1736 ms, Memory: 227624 KB -BaekjoonHub

--- 백준/Gold/9024. 두 수의 합/두 수의 합.java
+++ 백준/Gold/9024. 두 수의 합/두 수의 합.java
@@ -2,35 +2,37 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+	static StringBuilder sb = new StringBuilder();
+	static int N, K;
+	static int[] arr;
+
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 		int S = Integer.parseInt(br.readLine());
-
 		for(int iter = 0; iter < S; iter++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			int[] arr = new int[N];
+			N = Integer.parseInt(st.nextToken());
+			K = Integer.parseInt(st.nextToken());
+			arr = new int[N];
 			st = new StringTokenizer(br.readLine());
 			for(int i = 0; i < N; i++) arr[i] = Integer.parseInt(st.nextToken());
-			sb.append(findAnswer(arr, K)).append("\n");
+			findAnswer();
 		}
 		System.out.print(sb);
 	}
 
-	static int findAnswer(int[] arr, int target) {
+	static void findAnswer() {
 		Arrays.sort(arr);
-		int cnt = 0, min = Integer.MAX_VALUE, startIdx = 0, endIdx = arr.length-1;
+		int cnt = 0, min = Integer.MAX_VALUE, startIdx = 0, endIdx = N-1;
 		while(startIdx < endIdx) {
-			int sum = arr[startIdx] + arr[endIdx], current = Math.abs(target - sum);
+			int sum = arr[startIdx] + arr[endIdx], current = Math.abs(K - sum);
 			if(min > current) {
 				min = current;
 				cnt = 1;
 			} else if(min == current) cnt++;
-			if(target < sum) endIdx--;
+			if(K < sum) endIdx--;
 			else startIdx++;
 		}
-		return cnt;
+		sb.append(cnt).append("\n");
 	}
 }
