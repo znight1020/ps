9bf75edb66efb1d56b9d1aacf41c91fa9b706f3f
이현수 <znight1020@naver.com>
2025-02-08 03:41:15 +0900

    [Silver I] Title: 스티커, Time: 668 ms, Memory: 111616 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.java
+++ 백준/Silver/9465. 스티커/스티커.java
@@ -1,42 +1,40 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int[][] sticker;
-    static int[][] dp;
-    static boolean[][] vtd;
-    static int n;
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
 
-        for(int t = 1; t <= T; t++){
-            n = Integer.parseInt(br.readLine());
-            sticker = new int[2][n];
-            dp = new int[2][n];
-            vtd = new boolean[2][n];
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringBuilder sb = new StringBuilder();
+    int T = Integer.parseInt(br.readLine());
 
-            for(int i = 0; i < 2; i++){
-                st = new StringTokenizer(br.readLine());
-                for(int j = 0; j < n; j++) sticker[i][j] = Integer.parseInt(st.nextToken());
-            }
-            
-            dp[0][0] = sticker[0][0];
-            dp[1][0] = sticker[1][0];
-            solution();
-            sb.append(Math.max(dp[0][n-1], dp[1][n-1])).append("\n");
+    for(int t = 0; t < T; t++) {
+      int n = Integer.parseInt(br.readLine());
+      int[][] arr = new int[2][n];
+      int[][] dp = new int[2][n];
+
+      for(int i = 0; i < 2; i++) {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int j = 0; j < n; j++) arr[i][j] = Integer.parseInt(st.nextToken());
+      }
+
+      dp[0][0] = arr[0][0];
+      dp[1][0] = arr[1][0];
+      if(n == 1) sb.append(Math.max(dp[0][0], dp[1][0])).append("\n");
+      else {
+        dp[0][1] = dp[1][0] + arr[0][1];
+        dp[1][1] = dp[0][0] + arr[1][1];
+        if(n == 2) sb.append(Math.max(dp[0][1], dp[1][1])).append("\n");
+        else {
+          for(int j = 2; j < n; j++) {
+            dp[0][j] = arr[0][j] + Math.max(dp[1][j-1], dp[1][j-2]);
+            dp[1][j] = arr[1][j] + Math.max(dp[0][j-1], dp[0][j-2]);
+          }
+          sb.append(Math.max(dp[1][n-1], dp[0][n-1])).append("\n");
         }
-        System.out.print(sb);
+      }
     }
 
-    static void solution(){
-        for(int i = 1; i < n; i++){
-            dp[0][i] = Math.max(sticker[0][i] + dp[1][i-1], Math.max(dp[0][i-1], dp[1][i-1]));
-            dp[1][i] = Math.max(sticker[1][i] + dp[0][i-1], Math.max(dp[1][i-1], dp[0][i-1]));
-        }
-    }
+    System.out.print(sb);
+  }
 }
